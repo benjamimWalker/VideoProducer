914555c1ee368663cb7883952ec9924000a1b979
benjamimWalker <55597702+benjamimWalker@users.noreply.github.com>
2019-10-08 18:01:20 -0300

    Update gettags.py

--- gettags.py
+++ gettags.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from time import sleep
-import pyperclip
-def get_tags(term):
-    chrome = webdriver.Chrome(executable_path='/home/benjamim/Documentos/chromedriver')
 
+def get_tags_text(term):
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+
+    chrome = webdriver.Chrome(executable_path='/home/benjamim/Documentos/chromedriver', chrome_options=options)
     chrome.get('https://tagsyoutube.com/home')
     sleep(1.8)
 
@@ -17,5 +19,16 @@ def get_tags(term):
     chrome.find_element_by_id('copyTags').click()
     sleep(0.2)
     chrome.find_element_by_id('copytoclipboard').click()
-    sleep(0.7)
-    sleep(80)
+    sleep(1)
+    frase = chrome.find_element_by_id('finalTags').text.replace('x', ',').replace(' , ', ',')
+    frase = frase[0:-2]
+
+    return frase
+
+
+def tratafrase(term):
+    frase = get_tags_text(term)
+    frase = frase.split(',')
+    while len(frase) > 19:
+        frase.pop()
+    return ','.join(frase)
